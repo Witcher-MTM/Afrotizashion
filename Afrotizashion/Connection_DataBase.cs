fa44d1623e02ad4fee59b5ca0fd81598f9c693c1
Witcher-MTM <phantomit228@gmail.com>
2022-02-28 23:22:49 +0300

    Add project files.

---
+++ Afrotizashion/Connection_DataBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlClient;
+
+namespace Authentication
+{
+    public class Connection_DataBase
+    {
+        public static SqlConnection conn { get; set; }
+        public static bool isConnect { get; set; }
+        public Connection_DataBase()
+        {
+        }
+        public static bool Connect()
+        {
+            if (conn == null)
+            {
+                conn = new SqlConnection("Data Source=SQL5102.site4now.net;Initial Catalog=db_a838e6_gray;User Id=db_a838e6_gray_admin;Password=qwerty20039");
+            }
+            isConnect = false;
+            try
+            {
+                conn.Open();
+                isConnect = true;
+            }
+            catch (System.Exception)
+            {
+                isConnect = false;
+            }
+            return isConnect;
+        }
+    }
+}
---
+++ Afrotizashion/User.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+namespace Authentication
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Pass { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public DateTime dateRegistr { get; set; }
+        public User()
+        {
+            Login = String.Empty;
+            Pass = String.Empty;
+            BirthDay = DateTime.MinValue;
+            Email = String.Empty;
+        }
+
+        public User(string login, string pass, DateTime BD, string email, DateTime registr)
+        {
+            this.Login = login;
+            this.Pass = pass;
+            this.BirthDay = BD;
+            this.Email = email;
+            this.dateRegistr = registr;
+        }
+        public void GenToken()
+        {
+            byte[] time = BitConverter.GetBytes(DateTime.UtcNow.ToBinary());
+            byte[] key = Guid.NewGuid().ToByteArray();
+            this.Token = Convert.ToBase64String(time.Concat(key).ToArray());
+        }
+        public bool ValidDates()
+        {
+            if (ValidPass() && ValidEmail() && ValidLogin())
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool ValidPass()
+        {
+            if (Pass.Length > 6)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool ValidLogin()
+        {
+            if (Login.Length > 3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool ValidEmail()
+        {
+            var checkEmail = new EmailAddressAttribute();
+            if (checkEmail.IsValid(Email))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
